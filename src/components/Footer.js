3447b3eb8195991c1fce6d6b28d40a80a8adc6fa
Sagnik Sahoo <70798495+devgossips@users.noreply.github.com>
2020-11-24 09:13:12 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import '../css/components.css'
 function Footer() {
     return (
         <div className = "Footer container">
-            <p>Made with <span role="img" aria-label="heart"> ❤️ </span>&nbsp; by Aritro Paul</p>
+            <p>Made with <span role="img" aria-label="heart"> ❤️ </span>&nbsp; by Sagnik Sahoo</p>
         </div>
     )
 }
