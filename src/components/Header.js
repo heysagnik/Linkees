0e0e7eba50e65f8f0eda85df525b556172f2e7de
Sagnik Sahoo <70798495+devgossips@users.noreply.github.com>
2020-11-24 21:22:28 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,16 @@ function Header() {
     return (
         <div className = "Header container">
             <div className = "ten columns">
-             . sagnik
+             Sagnik
+           
             </div>
            
             
-            {/* <div className = "two columns">
+            <div className = "two columns">
                 <button>
                     <feather className="icons sun"></feather>
                 </button>
-            </div> */}
+            </div> 
         </div>
     )
 }
