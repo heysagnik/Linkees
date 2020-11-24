0ee4c01b38cfe848a681f05cfcde71644d60d40b
Sagnik Sahoo <70798495+devgossips@users.noreply.github.com>
2020-11-24 11:37:50 +0530

    Update items.js

--- src/assets/items.js
+++ src/assets/items.js
@@ -1,7 +1,7 @@
 import website from '../assets/website.png'
 import instagram from '../assets/instagram.png'
 import appstore from '../assets/IMG_20201124_112759.png'
-import youtube from '../assets/yt_logo_rgb_light.png'
+import youtube from '../assets/IMG_20201124_113450.png'
 import dribbble from '../assets/dribbble.png'
 import github from '../assets/github.png'
 import telegram from '../assets/IMG_20201124_104554.png'
@@ -14,7 +14,7 @@ const items = [{
 },
 {
     "title": "GitHub",
-    "subtitle": "@devgossips| 🏠 of my open-source projects",
+    "subtitle": "@devgossips | 🏠 of my open-source projects",
     "image": github,
     "link": "https://github.com/devgossips"
 },
@@ -26,26 +26,26 @@ const items = [{
 },
 {
     "title": "Apps",
-    "subtitle": "Hub of my awesome Apps",
+    "subtitle": "Hub of my awesome🔥 Apps",
     "image": appstore,
     "link": "#"
 },
 {
     "title": "YouTube",
-    "subtitle": "@Sagnik Sahoo|Official channel of mine",
+    "subtitle": "@Sagnik Sahoo | Official channel of mine",
     "image": youtube,
     "link": "https://www.youtube.com/channel/UCOUa9hvd4sJWQWQRIQctbSg"
 },
 
 {
     "title": "Dribbble",
-    "subtitle": "@virtuonic|Hub to the Shots of my 😎 apps ",
+    "subtitle": "@virtuonic | Hub to the Shots of my 😎 apps ",
     "image": dribbble,
     "link": "https://dribbble.com/virtuonic"
 },
 {
     "title": "Telegram",
-    "subtitle": "@virtuonic |Chat with me instantly, Don't hesitate! ",
+    "subtitle": "@virtuonic | Chat with me instantly, Don't hesitate! ",
     "image": telegram,
     "link": "https://telegram.me/virtuonic"
 }]
