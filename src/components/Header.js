0fa1f4a81116d468177f3271292da2e36d11364f
Sagnik Sahoo <70798495+devgossips@users.noreply.github.com>
2020-11-24 14:37:07 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,14 @@ import React from 'react'
 import '../css/skeleton.css'
 import '../css/normalize.css'
 import '../css/components.css'
-
+import style from '../css/components.css';
 function Header() {
     return (
         <div className = "Header container">
             <div className = "ten columns">
              . sagnik
             </div>
-           <div className = "" >
+           <div>
            <img className={style.profile} src="https://avatars0.githubusercontent.com/u/28679360?s=460&u=043e532cfc62169437853995504c073352eca53f&v=4" alt="Imanol Suarez Foto" />
             <p className={style.username}>
             Hi👋
