7dfce4e7cfbe5b686b860454a23d28df415481e3
JatinBhargava <jatinbhargava4@gmail.com>
2021-10-31 01:47:19 +0530

    Logo-Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,15 +3,14 @@ import React from 'react'
 import '../css/skeleton.css'
 import '../css/normalize.css'
 import '../css/components.css'
+import logo from '../Logo/Logo-2.png'
+
 
 function Header() {
     return (
         <div className = "Header container">
             <div className = "ten columns">
-            <img src="https://avatars.githubusercontent.com/u/70798495?v=4" alt="logo" />
-  &nbsp;&nbsp;&nbsp;
-            <h2>Sagnik </h2>
-           
+            <img src={logo} alt="logo" />
             </div>
            
             
