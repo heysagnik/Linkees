4c4dcd33a6f2b2c13e8941ffd908c4ba136141d2
ashalfarhan <ashalfarhan8@gmail.com>
2021-11-02 16:27:38 +0700

    add logic for theme switcher

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,34 @@
 import React from 'react'
+import SunIcon from './Icons/SunIcon'
+import MoonIcon from './Icons/MoonIcon'
 import logo from '../assets/logo.png'
 import '../css/skeleton.css'
 import '../css/normalize.css'
 import '../css/components.css'
 
 function Header() {
-    return (
-        <div className = "Header container">
-            <div className = "ten columns">
-            <img src={logo} alt="logo" />
-  &nbsp;&nbsp;&nbsp;
-            <h2><b>Sagnik </b></h2>
-        
-            </div>
-           
-        </div>
-    )
+  const [theme, setTheme] = React.useState(() => document.body.getAttribute('data-theme') ?? 'dark')
+  React.useEffect(() => {
+    document.body.setAttribute('data-theme', theme)
+  }, [theme])
+  const handleSwitchTheme = () => {
+    setTheme(isDark ? 'light' : 'dark')
+  }
+  const isDark = theme === 'dark'
+  return (
+    <div className="Header container">
+      <div className="ten columns Header__inner">
+        <img src={logo} alt="logo" />
+        &nbsp;&nbsp;&nbsp;
+        <h2>
+          <b>Sagnik </b>
+        </h2>
+      </div>
+      <button className="switch-theme-button" onClick={handleSwitchTheme}>
+        {isDark ? <SunIcon color="white" /> : <MoonIcon />}
+      </button>
+    </div>
+  )
 }
 
 export default Header
