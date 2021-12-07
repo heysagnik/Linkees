9410a2bf11d31d2d54b3c3e7fabeddb59a2c231d
Sagnik Sahoo <sahoosagnik1@gmail.com>
2021-12-07 14:10:17 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ import '../css/components.css'
 function Footer() {
     return (
         <div className="Footer container">
-            <p>Made with <span role="img" aria-label="heart">❤️</span> &nbsp; by &nbsp; <a href={"https://twitter.com/heysagnik"}>Sagnik Sahoo</a></p>
+            <p>Made with <svg viewBox="0 0 1792 1792" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" style="height: 0.8rem;"><path d="M896 1664q-26 0-44-18l-624-602q-10-8-27.5-26T145 952.5 77 855 23.5 734 0 596q0-220 127-344t351-124q62 0 126.5 21.5t120 58T820 276t76 68q36-36 76-68t95.5-68.5 120-58T1314 128q224 0 351 124t127 344q0 221-229 450l-623 600q-18 18-44 18z" fill="#e25555"></path></svg> &nbsp; by &nbsp; <a href={"https://twitter.com/heysagnik"}>Sagnik Sahoo</a></p>
         </div>
     )
 }
