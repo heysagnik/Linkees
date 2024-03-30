4b59ae7973a2ab67d8c12eeface374dfc142558e
Avalynn <100826194+avalynndev@users.noreply.github.com>
2024-03-30 05:46:33 +0000

    remove tailwindcss and add padding

--- packages/Linkees/src/components/components/Footer.tsx
+++ packages/Linkees/src/components/components/Footer.tsx
@@ -5,7 +5,7 @@ import '../../css/components.css';
 
 function Footer(): JSX.Element {
   return (
-    <div className="Footer container pb-10">
+    <div className="Footer container">
       <p>
         Made with <span className="heart">❤️</span> by <a href={'https://twitter.com/heysagnik'}>Sagnik Sahoo</a>
       </p>
